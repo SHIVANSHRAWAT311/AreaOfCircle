b53000ae30d4c3bbef76e703d1d8f112dd0576db
KAKASU <editzzeeco@gmail.com>
2025-08-02 22:52:38 +0530

    Add files via upload

---
+++ circlearea.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class circlearea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER RADIUS");
+        double radius = sc.nextInt();
+        double AREA = (3.14 * radius * radius);
+        System.out.println(AREA);
+
+
+
+
+    }
+}
+
+
+
